1bddf111d638dd9e6b1704a472803bd211ddbd70
Rebecca Cox <rebeccacox@Rebeccas-MacBook-Air-2.local>
2026-03-08 09:27:39 +0000

    30 working on ios simulator

---
+++ ata/lib/conditional_io.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+bool determineIsIncomingResetPassword(){
+  return false;
+}
+
+Widget showPayPalButton(String userId){
+  throw UnimplementedError('Unsupported');
+}
+
+void webListenForMessage(){
+  throw UnimplementedError('Unsupported');
+}
